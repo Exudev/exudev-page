c5bea510d3d60e9742610ce6674afb1fd8f29958
Exudev <pablo.euardo10@gmail.com>
2024-03-16 01:50:38 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 'use client'
 import Head from "next/head";
+import Image from 'next/image'
 import { useState,  } from "react";
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {
   AiFillTwitterCircle,
   AiFillLinkedin,
   AiFillGithub,
-
-  
 } from "react-icons/ai";
+import {Card } from "./components/ProjectCard"
 
 export default function Home() {
    const [darkMode, setDarkMode] = useState(false);
+
+ 
   return (
     <div className={darkMode ? "dark": ""}>
       <Head>
@@ -20,9 +22,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
-        <section className="min-h-screen">
+        <section id="main" className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between  dark:text-white">
-            <h1 className="text-xl font-KING668">developed by exudev</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -33,8 +34,8 @@ export default function Home() {
 
               <li>
                 <a
-                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                   href="#"
+                   className="bg-gradient-to-r from-purple-900 to-violet-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                   href="./props/exudev-vitae.pdf" download ="exudev-vitae"
                 >
                   Resume
                 </a>
@@ -42,9 +43,20 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10" >
-            <h2 className = "text-5xl py-2 text-teal-600 font-medium">Pablo Edu Diaz</h2>
+          <div  className="flex justify-center items-center">
+      <div className="rounded-full overflow-hidden" >
+        <Image
+          src="/pp.jpeg"
+          alt="Profile Picture"
+          width={350}
+          height={350}
+          className=""
+        />
+      </div>
+    </div>
+            <h2 className = "text-5xl py-2 text-purple-900 font-medium">Pablo Edu Diaz</h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">Developer</h3>
-            <p className="text-md py-5 leading-8 text-white">
+            <p className="text-md py-5 leading-8 text-black   dark:text-white">
               {" "}
               Im Pablo Diaz, known online as Exudev. I have experience in
               software development as a full stack programmer, standing out in
@@ -57,7 +69,7 @@ export default function Home() {
               one of my strengths.
               </p>
 
-              <p className="text-md py-5 leading-8 text-white">
+              <p className="text-md py-5 leading-8 text-black   dark:text-white">
               I am always willing to take on new challenges
               and learn new technologies in this constant and dynamic field. In
               addition to my career in software development, I have a variety of
@@ -68,17 +80,18 @@ export default function Home() {
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gra-600">
-            <a href="https://www.linkedin.com/in/pablo-eduardo-diaz-hernandez-a1b457183/">  <AiFillLinkedin /></a>
-            <a href="https://twitter.com/pabloedudh"> <AiFillTwitterCircle/></a>
-            <a href="https://github.com/Exudev">  <AiFillGithub/> </a>
+            <a href="https://www.linkedin.com/in/pablo-eduardo-diaz-hernandez-a1b457183/">  <AiFillLinkedin className="icon text-black-500 hover:text-purple-950" /></a>
+            <a href="https://twitter.com/pabloedudh"> <AiFillTwitterCircle  className="icon text-black-500 hover:text-purple-950"/></a>
+            <a href="https://github.com/Exudev">  <AiFillGithub className="icon text-black-500 hover:text-purple-700"/> </a>
           </div>
           <div className="">
 
           </div>
         </section>
-        <section>
-          <div>
-            <h3>few of my projects</h3>
+        <section id="projects">
+          <div className="text-center p-10" >
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">Few of my projects</h3>
+        <Card description="Juego de mesa con cartas, utilice JS" imageSrc="/CartasLocas.png" title="Cartas Locas"/>
           </div>
         </section>
       </main>
